aa597d8cfdd7c954dd2866d4d5df05f4140bfb90
Kevyn Marins <kevynmarins@gmail.com>
2020-11-05 15:31:28 +0000

    add ContactsTest

---
+++ tests/com/company/ContactsTest.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactsTest {
+
+    @Test
+    void Email() {
+        Contacts contacts = new Contacts();
+        assertEquals(contacts.getEmail(), null);
+        contacts.setEmail("person@someplace.com");
+        assertEquals(contacts.getEmail(), "person@someplace.com");
+    }
+
+    @Test
+    void PhoneNumber() {
+        Contacts contacts = new Contacts();
+        assertEquals(contacts.getPhoneNumber(), 0);
+        contacts.setPhoneNumber(111111111);
+        assertEquals(contacts.getPhoneNumber(), 111111111);
+    }
+}
