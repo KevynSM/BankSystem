8c69c7fae114dfc5dc20b1da9237b0420d195315
Kevyn Marins <kevynmarins@gmail.com>
2020-11-05 12:26:26 +0000

    add Address class

--- src/com/company/Address.java
+++ src/com/company/Address.java
@@ -1,4 +1,31 @@
 package com.company;
 
 public class Address {
+    private String street;
+    private String postalCode;
+    private String city;
+    private String county;
+
+    Address(String street, String postalCode, String city, String country) {
+        this.street = street;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.county = country;
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+
+    public String getPostalCode() {
+        return this.postalCode;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getCountry() {
+        return this.county;
+    }
 }
