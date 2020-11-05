0ae8a01b46083f46d1e37eee4dcca2df7b8f1d7e
Kevyn Marins <kevynmarins@gmail.com>
2020-11-05 14:08:59 +0000

    add Contacts class

--- src/com/company/Contacts.java
+++ src/com/company/Contacts.java
@@ -1,4 +1,22 @@
 package com.company;
 
 public class Contacts {
+    private String email;
+    private int phoneNumber;
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    public String getEmail () {
+        return this.email;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPhoneNumber() {
+        return this.phoneNumber;
+    }
 }
