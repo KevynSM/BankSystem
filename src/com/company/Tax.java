edac730f8755ed057340f036873b16814826957c
Kevyn Marins <kevynmarins@gmail.com>
2020-11-05 13:39:00 +0000

    Add Tax class

---
+++ src/com/company/Tax.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Tax {
+    private double value = 0.49;
+
+    public Tax() {}
+
+    public double getValue() {
+        return this.value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
