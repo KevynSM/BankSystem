22babf408eeaec25a0e294bef0524320c6e35ba0
DanielAlmeida309 <58252911+DanielAlmeida309@users.noreply.github.com>
2020-11-06 22:51:18 +0000

    create package Controllers and class Bank

---
+++ src/D3/controllers/Bank.java
@@ -0,0 +1,15 @@
+package D3.controllers;
+
+import D3.models.Tax;
+import D3.models.Account;
+import D3.models.Client;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Bank {
+    Tax tax = new Tax();
+    List<Account> accounts = new LinkedList<Account>();
+    List<Client> clients = new LinkedList<Client>();
+
+}
