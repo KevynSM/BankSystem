26953f2112456e83f9abcc0e6ff4db20b5d5ee0c
Kevyn Marins <kevynmarins@gmail.com>
2020-11-06 20:27:42 +0000

    Add comments to the Tax class

--- src/D3/models/Tax.java
+++ src/D3/models/Tax.java
@@ -1,14 +1,32 @@
 package D3.models;
 
+/** Represents a Tax in a Bank.
+ * @author Diurno - Grupo 3.
+ * @author Daniel Almeida.
+ * @author João Conduto.
+ * @author Kevyn Marins.
+ * @author Tiago Leitão.
+ */
 public class Tax {
     private double value = 0.49;
 
+    /** Creates a new tax object
+     *
+     */
     public Tax() {}
 
+    /** Returns the value of the tax.
+     *
+     * @return the value of the tax.
+     */
     public double getValue() {
         return this.value;
     }
 
+    /** Sets a new tax value
+     *
+     * @param value The new tax value.
+     */
     public void setValue(double value) {
         this.value = value;
     }
